030940d76c217d64137bcd4f4ba2ac7d4cdb4f0b
Brook Huang <110752897@qq.com>
2014-12-05 17:09:18 +0800

    add straigt page class

---
+++ Xy.Web/Page/StraightPage.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Xy.Web.Page {
+    public sealed class StraightPage : PageAbstract {
+        public override void onGetRequest() {
+            SetNewTemplate(string.Concat(WebSetting.PageDir, URL.File));
+        }
+    }
+}
